b912e99d626be11b1c0d39aa5099bffaf6eaa715
John Restrepo <marchainca@gmail.com>
2024-12-29 14:24:14 -0500

    create attendance record

--- src/attendance/attendance.controller.ts
+++ src/attendance/attendance.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { AttendanceService } from './attendance.service';
 
 @Controller('attendance')
-export class AttendanceController {}
+export class AttendanceController {
+    constructor(private readonly attendanceService: AttendanceService) {}
+
+     // Ruta para identificar al integrante
+    @Post('identify')
+    async identifyIntegrante(@Body('rostroHash') rostroHash: string): Promise<any> {
+        return await this.attendanceService.identifyIntegrante(rostroHash);
+    }
+
+    // Ruta para registrar asistencia
+    @Post('register')
+    async registerAttendance(
+        @Body('integranteId') integranteId: string,
+        @Body('actividad') actividad: string,
+    ): Promise<string> {
+        return await this.attendanceService.registerAttendance(integranteId, actividad);
+    }
+}
--- src/attendance/attendance.service.ts
+++ src/attendance/attendance.service.ts
@@ -1,4 +1,38 @@
-import { Injectable } from '@nestjs/common';
+import { Firestore } from '@google-cloud/firestore';
+import { Injectable, NotFoundException } from '@nestjs/common';
 
 @Injectable()
-export class AttendanceService {}
+export class AttendanceService {
+    private firestore: Firestore
+    constructor(){
+        this.firestore = new Firestore();
+    }
+
+    // Buscar integrante por el hash facial
+  async identifyIntegrante(rostroHash: string): Promise<any> {
+    const integrantesRef = this.firestore.collection('integrantes');
+    const querySnapshot = await integrantesRef.where('rostroHash', '==', rostroHash).get();
+
+    if (querySnapshot.empty) {
+      throw new NotFoundException('No se encontró un integrante con el rostro escaneado.');
+    }
+
+    // Retornar los datos del primer integrante encontrado
+    const integrante = querySnapshot.docs[0].data();
+    return integrante;
+  }
+
+  // Registrar asistencia
+  async registerAttendance(integranteId: string, actividad: string): Promise<string> {
+    const asistenciasRef = this.firestore.collection('asistencias');
+    const newAttendanceRef = asistenciasRef.doc();
+
+    await newAttendanceRef.set({
+      integranteId,
+      fecha: new Date().toISOString(),
+      actividad,
+    });
+
+    return `Asistencia registrada exitosamente para el integrante ${integranteId}`;
+  }
+}
